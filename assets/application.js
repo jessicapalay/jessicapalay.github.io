1da93bf05e15465a84d25cd0d7a40cde73f57721
Jessica Palay <jessica.c.palay@gmail.com>
2013-08-20 19:21:53 -0500

    added more jQuery elements

--- assets/application.js
+++ assets/application.js
@@ -4,13 +4,59 @@ $(document).ready(function() {
 		console.log("click");
 	});
 
-	$(".navigation a").click(function() {
+	$("#about").click(function() {
 		$(".about-page").show();
-		$(".page").hide();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".work-page").hide();
+		$(".contact-page").hide();
 	});
 
-	$("navigation a").click(function() {
+	$("#work").click(function() {
 		$(".work-page").show();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".about-page").hide();
+		$(".contact-page").hide();
+	});
+
+	$("#contact").click(function() {
+		$(".contact-page").show();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".about-page").hide();
+		$(".work-page").hide();		
+	});
+});
+
+$(document).ready(function() {
+	$(".navigation-1 a").click(function(event) {
+		event.preventDefault();
+		console.log("click");
+	});
+
+	$(".about-1").click(function() {
+		$(".about-page").show();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".work-page").hide();
+		$(".contact-page").hide();
+	});
+
+	$(".work-1").click(function() {
+		$(".work-page").show();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".about-page").hide();
+		$(".contact-page").hide();	
+	});
+
+	$(".contact-1").click(function() {
+		$(".contact-page").show();
+		$(".page").slideUp();
+		$(".navigation").slideUp();
+		$(".about-page").hide();
+		$(".work-page").hide();		
 	});
 });
 
